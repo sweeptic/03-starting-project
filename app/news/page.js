b7470d689aa028f4fa1115a56534b680a9e6c71d
Attila Horvath <sweeptic@gmail.com>
2025-02-27 10:49:59 +0100

    008 Working with Parallel Routes & Nested Routes

--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,32 @@
+// import { DUMMY_NEWS } from '@/dummy-news';
+// import Link from 'next/link';
+
+// export default function NewsPage() {
+//   const links = DUMMY_NEWS.map((newsItem) => (
+//     <li key={newsItem.id}>
+//       <Link href={`/news/${newsItem.slug}`}>
+//         {newsItem.title}
+//         <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+//         <span>{newsItem.title}</span>
+//       </Link>
+//     </li>
+//   ));
+
+//   return (
+//     <div id="home">
+//       <ul className="news-list">{links}</ul>
+//     </div>
+//   );
+// }
+
+import NewsList from '@/components/news-list';
 import { DUMMY_NEWS } from '@/dummy-news';
-import Link from 'next/link';
 
 export default function NewsPage() {
-  const links = DUMMY_NEWS.map((newsItem) => (
-    <li key={newsItem.id}>
-      <Link href={`/news/${newsItem.slug}`}>
-        {newsItem.title}
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-        <span>{newsItem.title}</span>
-      </Link>
-    </li>
-  ));
-
   return (
-    <div id="home">
-      <ul className="news-list">{links}</ul>
-    </div>
+    <>
+      <h1>News Page</h1>
+      <NewsList news={DUMMY_NEWS} />
+    </>
   );
 }
