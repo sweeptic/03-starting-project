2039de56dae264195d40677953f4ccabea65abb4
Attila Horvath <sweeptic@gmail.com>
2025-02-28 10:20:15 +0100

    018 Navigating Programmatically

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,17 +1,25 @@
+'use client';
+
 import { DUMMY_NEWS } from '@/dummy-news';
-import { notFound } from 'next/navigation';
+import { notFound, useRouter } from 'next/navigation';
 
 export default function InterceptedImagePage({ params }) {
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug);
+  const router = useRouter();
 
   if (!newsItem) {
     notFound();
   }
 
+  const clickHandler = () => {
+    console.log('click');
+    router.back();
+  };
+
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={clickHandler} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
