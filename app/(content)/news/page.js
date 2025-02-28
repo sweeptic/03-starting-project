466d7e5029d29b8be69c6799e45eecca6df6b51c
Attila Horvath <sweeptic@gmail.com>
2025-02-28 13:43:02 +0100

    004 Option 2 Server-side Data Fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,65 +1,59 @@
-'use client';
+// 'use client';
 
 import NewsList from '@/components/news-list';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  //   const [isLoading, setIsLoading] = useState(false);
+  //   const [error, setError] = useState();
+  //   const [news, setNews] = useState();
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-      if (!response.ok) {
-        setError('Failed to fetch news.');
-        setIsLoading(false);
-      }
+  const response = await fetch('http://localhost:8080/news');
 
-      //   console.log('response', response.body);
-      //   console.log('RUN 1');
-      const news = await response.json(); // you can let it do it's stuff in the background without blocking
-      //   console.log('RUN 2');
-
-      //   const doParse = () => {
-      //     console.log('RUN 1');
-      //     const data = response.text();
-      //     console.log('RUN 2');
-      //     return data;
-      //   };
-
-      //   const news = JSON.parse(await doParse()); // is blocking.
-
-      //   console.log('RUN 2');
-      //   console.log('DEBUG', news);
-      //   console.log('RUN 3');
-
-      setNews(news);
-      setIsLoading(false);
-    };
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error('Failed to fetch news.');
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+  const news = await response.json();
 
-  let newsContent;
+  //   useEffect(() => {
+  //   const fetchNews = async () => {
+  //   setIsLoading(true);
+  //   const response = await fetch('http://localhost:8080/news');
+  //   if (!response.ok) {
+  //   setError('Failed to fetch news.');
+  //   setIsLoading(false);
+  // }
 
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  //   const news = await response.json(); // you can let it do it's stuff in the background without blocking
+  //   const news = JSON.parse(await response.text()); // is blocking.
+
+  //     setNews(news);
+  //     setIsLoading(false);
+  //   };
+
+  //   fetchNews();
+  //   }, []);
+
+  //   if (isLoading) {
+  //     return <p>Loading...</p>;
+  //   }
+
+  //   if (error) {
+  //     return <p>{error}</p>;
+  //   }
+
+  //   let newsContent;
+
+  //   if (news) {
+  //   newsContent = <NewsList news={news} />;
+  //   }
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      {/* {newsContent} */}
+      <NewsList news={news} />
     </>
   );
 }
